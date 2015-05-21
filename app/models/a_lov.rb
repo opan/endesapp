9ac92a4e820930daad7ce0e4e8149e46311baa0a
opan <opan.neutron@gmail.com>
2015-05-21 23:30:39 +0700

    20150521 2330 om

--- app/models/a_lov.rb
+++ app/models/a_lov.rb
@@ -5,7 +5,7 @@ class ALov < ActiveRecord::Base
   #      lov_params, created_by, created_at, updated_at
 
   validates_uniqueness_of :lov_id, messager: "Duplicate lov id."
-  validates_presence_of :lov_id, :lov_name, :lov_cat, :lov_parent,  
+  validates_presence_of :lov_id, :lov_name, :lov_cat,  
                         :created_by, :created_at, :updated_at
   validates_inclusion_of :is_master_detail, in: [true, false], if: :sm_default_value
   after_initialize :sm_default_value
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+p "Deleting all data on table a_lovs"
+ALov.delete_all
+
+p "Seeding table a_lovs with default values."
+ALov.create!(lov_id: 1, lov_name: "Pria", lov_cat: "gender_cat", is_master_detail: false, created_by: "Endesapp", created_at: Date.today, updated_at: Date.today)
+ALov.create!(lov_id: 2, lov_name: "Wanita", lov_cat: "gender_cat", is_master_detail: false, created_by: "Endesapp", created_at: Date.today, updated_at: Date.today)
+ALov.create!(lov_id: 3, lov_name: "Triple Des", lov_cat: "enc_type", is_master_detail: false, created_by: "Endesapp", created_at: Date.today, updated_at: Date.today)
